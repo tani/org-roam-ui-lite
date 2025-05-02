7a2ab55c39131dd9db04eb57431979dfea756355
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-05-02 15:28:36 +0900

    Add emacs mode

--- build.mjs
+++ build.mjs
@@ -1,11 +1,13 @@
 import slugify from "slugify";
-await $`rm -rf dist`
-await $`mkdir -p dist/server dist/client dist/licenses`
-await $`cp -vR packages/client/dist dist/client/dist`
-await $`cp -vR packages/server/dist dist/server/dist`
+
+await $`rm -rf dist`;
+await $`mkdir -p dist/server dist/client dist/licenses`;
+await $`cp -vR emacs dist/emacs`;
+await $`cp -vR packages/client/dist dist/client/dist`;
+await $`cp -vR packages/server/dist dist/server/dist`;
 const all = JSON.parse(await $`license-checker --json`);
-await $`mkdir -p dist/licenses`
-for(const [key, value] of Object.entries(all)) {
-  if(value.licenseFile)
-    await $`cp ${value.licenseFile} dist/licenses/${slugify(key, { strict: true })}`
+await $`mkdir -p dist/licenses`;
+for (const [key, value] of Object.entries(all)) {
+	if (value.licenseFile)
+		await $`cp ${value.licenseFile} dist/licenses/${slugify(key, { strict: true })}`;
 }

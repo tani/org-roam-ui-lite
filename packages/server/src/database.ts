1468c09032077c704f20c55cb4140c9e74401dce
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-05-02 12:07:44 +0900

    format code

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -1,10 +1,10 @@
-import initSqlJs from "sql.js/dist/sql-asm.js";
-import { drizzle } from "drizzle-orm/sql-js";
-import * as schema from "./schema.ts";
 import * as fs from "node:fs/promises";
+import { drizzle } from "drizzle-orm/sql-js";
+import initSqlJs from "sql.js/dist/sql-asm.js";
+import * as schema from "./schema.ts";
 
 const db_path = process.env.ORU_DB_PATH ?? `${process.cwd()}/database.db`;
-const SQL = await initSqlJs()
-const database = new SQL.Database(new Uint8Array(await fs.readFile(db_path)))
+const SQL = await initSqlJs();
+const database = new SQL.Database(new Uint8Array(await fs.readFile(db_path)));
 
 export const db = drizzle(database, { schema });

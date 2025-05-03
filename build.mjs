2df217492cad2a9b3fac3c203aa1628552ee19e2
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-05-03 13:05:32 +0900

    Add emacs backend

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@ import slugify from "slugify";
 
 await $`rm -rf dist`;
 await $`mkdir -p dist/{server,client,licenses,emacs}`;
-await $`cp -vR emacs/* dist/emacs/`;
+await $`cp -vR packages/emacs/* dist/emacs/`;
 await $`cp -vR packages/client/dist dist/client/dist`;
 await $`cp -vR packages/server/dist dist/server/dist`;
 const all = JSON.parse(await $`license-checker --json`);

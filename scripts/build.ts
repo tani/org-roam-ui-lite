1ac3c976769154c83c3260ccb3889c0c697a786e
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-05-11 21:16:14 +0900

    Use spinner

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,5 @@
 import slugify from "slugify";
-import { $ } from "zx";
+import { $, spinner } from "zx";
 
 await $`rm -rf dist`;
 await $`mkdir -p dist/{backend,frontend,emacs,licenses}`;
@@ -7,12 +7,21 @@ await $`cp README.org LICENSE.org dist/`;
 await $`cp -vR packages/emacs/* dist/emacs`;
 await $`cp -vR packages/frontend/dist dist/frontend/dist`;
 await $`cp -vR packages/backend/dist dist/backend/dist`;
-const output = await $`license-checker --json`;
-await $`mkdir -p dist/licenses`;
+
 interface Licenses {
 	licenseFile?: string;
 }
-for (const [key, value] of Object.entries(output.json<Licenses>())) {
-	if (value.licenseFile)
-		await $`cp ${value.licenseFile} dist/licenses/${slugify.default(key, { strict: true })}`;
-}
+
+await spinner("copying license files", async () => {
+	await $`mkdir -p dist/licenses`;
+	const output = await $`license-checker --json`;
+	const pairs = Object.entries(output.json<Licenses>());
+	await Promise.all(
+		pairs
+			.filter(([_, v]) => v.licenseFile)
+			.map(
+				([k, v]) =>
+					$`cp ${v.licenseFile} dist/licenses/${slugify.default(k, { strict: true })}`,
+			),
+	);
+});

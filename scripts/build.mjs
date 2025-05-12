38f9711310d6b205b59d54fd8d30da14440dfd04
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-05-12 09:04:21 +0900

    remove slugify

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,5 +1,3 @@
-import slugify from "slugify";
-
 await fs.remove("./dist");
 await fs.mkdirp("./dist");
 await fs.copy("README.org", "./dist/README.org");
@@ -10,13 +8,17 @@ await fs.copy("packages/frontend/dist", "./dist/frontend/dist");
 await fs.mkdirp("./dist/backend");
 await fs.copy("packages/backend/dist", "./dist/backend/dist");
 
+function slugify(s) {
+	return s.replace(/[^a-zA-Z0-9]+/g, "_");
+}
+
 await spinner("copying license files", async () => {
 	await fs.mkdirp("./dist/licenses");
 	const output = await $`license-checker --json`;
 	const pairs = Object.entries(output.json());
 	for (const [k, v] of pairs) {
 		if (v.licenseFile) {
-			const dest = `dist/licenses/${slugify.default(k, { strict: true })}`;
+			const dest = `dist/licenses/${slugify(k)}`;
 			await fs.copy(v.licenseFile, dest);
 		}
 	}

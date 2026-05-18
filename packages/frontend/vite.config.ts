8b7a9c22e917c8d4498024b8f6c17b6faf685e21
Masaya Taniguchi <masaya.taniguchi.phd@gmail.com>
2026-05-18 15:54:01 +0900

    build: use workspace rehype packages

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
 
-import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
@@ -9,16 +8,6 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 export default defineConfig({
 	base: "./",
 	plugins: [react(), viteSingleFile()],
-	resolve: {
-		alias: {
-			"rehype-mathjax": fileURLToPath(
-				new URL("../rehype-mathjax/src/browser.ts", import.meta.url),
-			),
-			"rehype-mermaid": fileURLToPath(
-				new URL("../rehype-mermaid/src/index.ts", import.meta.url),
-			),
-		},
-	},
 	server: {
 		port: 5173,
 		proxy: {
